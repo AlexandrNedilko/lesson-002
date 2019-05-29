9cb34190a54b3ff69056c4c78b80d3f903e40838
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-05-29 22:44:02 +0300

    Add class MainZero

---
+++ src/test/java/MainZero.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MainZero {
+
+    public static void main(String[] args){
+
+        for(int i=0; i<7; i++){
+            for(int j=0; j<10; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
